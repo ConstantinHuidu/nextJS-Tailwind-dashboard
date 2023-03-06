0a90d1f1b40820113af2a80a384d0da059cd569e
CHuidu <constantin.huidu@htsg.eu>
2023-03-06 17:43:34 +0200

    piechart refactor

--- components/expenses/components/RecentExpenses.jsx
+++ components/expenses/components/RecentExpenses.jsx
@@ -6,11 +6,15 @@ const RecentExpenses = ({ expenses }) => {
   return (
     <div className="w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-y-scroll">
       <h1 className="text-black font-bold text-2xl">Recent Expenses</h1>
-      <ul>
-        {expenses.map((expense) => (
-          <ExpenseCard key={expense._id} expense={expense} />
-        ))}
-      </ul>
+      {expenses.length > 0 ? (
+        <ul>
+          {expenses.map((expense) => (
+            <ExpenseCard key={expense._id} expense={expense} />
+          ))}
+        </ul>
+      ) : (
+        <p className="py-4">You don't have any expenses</p>
+      )}
     </div>
   );
 };
