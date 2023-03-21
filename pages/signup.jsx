eb29e1ab8def2161ab98bfe8c8e711197caa0ca5
ConstantinHuidu <constantin.huidu@gmail.com>
2023-03-21 13:37:35 +0200

    redirect on signup

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,8 +1,27 @@
-import Signup from "@/components/Signup";
 import React from "react";
+import { getSession } from "next-auth/react";
+import Signup from "@/components/Signup";
 
 const signup = () => {
   return <Signup />;
 };
+
+// === SERVER-SIDE REDIRECT IF USER IS AUTHENTICATED ===
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
 
 export default signup;
