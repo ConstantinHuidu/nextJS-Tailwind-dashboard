d0272a459cfe90f7f44d58b30aa56b79e1832cb6
ConstantinHuidu <60363402+ConstantinHuidu@users.noreply.github.com>
2023-04-03 17:20:32 +0300

    Transactions topbar (#21)
    * topControls updated
    
    * added more styles to topControls

--- components/transactions/components/TopControls.jsx
+++ components/transactions/components/TopControls.jsx
@@ -2,27 +2,31 @@ import React from "react";
 
 const TopControls = ({ onOpenModal, onOpenNewExpenseModal }) => {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-5 gap-4 p-4">
+    <div className="flex flex-col space-y-3  md:flex-row md:space-y-0 md:space-x-5 justify-between items-center p-4">
       <div
         onClick={onOpenNewExpenseModal}
-        className="lg:col-span-2 col-span-1 bg-white flex justify-around items-center w-full border p-2 md:p-4 rounded-lg cursor-pointer"
+        className="bg-white flex justify-around items-center w-3/4 md:w-1/2 border p-2 md:p-4 rounded-lg hover:bg-amber-50 duration-200 cursor-pointer group"
       >
-        <p className="text-xs md:text-2xl text-cyan-700 font-bold">
-          New transaction
+        <p className="text-xs md:text-lg lg:text-2xl text-gray-600 font-bold md:font-semibold">
+          Add new transaction
         </p>
-        <p className=" hidden bg-green-200 md:flex justify-center items-center p-2 md:px-5 rounded-lg">
-          <span className="text-green-700 text-xs md:text-lg">+</span>
+        <p className=" hidden bg-green-200 group-hover:bg-green-500 md:flex justify-center items-center p-2 md:px-5 rounded-lg duration-200">
+          <span className="text-green-700 group-hover:text-white text-xs font-bold md:text-xl duration-200">
+            +
+          </span>
         </p>
       </div>
       <div
         onClick={onOpenModal}
-        className="lg:col-span-2 col-span-1 bg-white flex justify-around items-center w-full border p-4 rounded-lg cursor-pointer"
+        className="bg-white flex justify-around items-center w-3/4 md:w-1/2 border p-2 md:p-4 rounded-lg hover:bg-amber-50 duration-200 cursor-pointer group"
       >
-        <p className="text-xs md:text-2xl text-cyan-700 font-bold">
-          New transaction category
+        <p className="text-xs md:text-lg lg:text-2xl text-gray-600 font-bold  md:font-semibold duration-300">
+          Add new category
         </p>
-        <p className="hidden bg-green-200 md:flex justify-center items-center p-2 md:px-5 rounded-lg">
-          <span className="text-green-700 text-xs md:text-lg">+</span>
+        <p className="hidden bg-green-200 group-hover:bg-green-500 md:flex justify-center items-center p-2 md:px-5 rounded-lg duration-200">
+          <span className="text-green-700 group-hover:text-white text-xs font-bold md:text-xl duration-200">
+            +
+          </span>
         </p>
       </div>
     </div>
