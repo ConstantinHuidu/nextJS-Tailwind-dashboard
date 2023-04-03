8d7e586c94a5709d696835e345cf729582a95608
ConstantinHuidu <60363402+ConstantinHuidu@users.noreply.github.com>
2023-04-03 16:52:16 +0300

    added burger menu and mobile navbar (#20)

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { MdLogout } from "react-icons/md";
 import { useSession, signOut } from "next-auth/react";
 import Logo from "../assets/images/Logo.png";
@@ -7,6 +7,11 @@ import Image from "next/image";
 
 const Header = () => {
   const { data: session, status } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleBurgerMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   const logoutHandler = () => {
     signOut();
@@ -15,14 +20,19 @@ const Header = () => {
   return (
     <div className="relative flex flex-row max-w-7xl mx-auto justify-between items-center md:items-center p-4">
       <span className="absolute bottom-0 left-5 right-5 h-0.5 bg-stone-300"></span>
-      <div className="flex justify-between items-start">
-        <div className="relative flex justify-center items-center w-20 h-20">
+      <div className="flex justify-center items-center">
+        <div
+          className="relative flex justify-center items-center text-6xl"
+          onClick={() => setIsOpen(false)}
+        >
           <Link href="/">
-            <Image alt="logo" src={Logo} fill className="scale-[4] ml-20" />
+            LOGO
+            {/* <Image alt="logo" src={Logo} fill className="scale-[4] ml-20" /> */}
           </Link>
         </div>
       </div>
 
+      {/* DESKTOP NAVBAR */}
       <div className="hidden md:flex justify-between items-center gap-16">
         {!session && (
           <Link
@@ -55,6 +65,55 @@ const Header = () => {
             </div>
           </>
         )}
+      </div>
+
+      {/* BURGER MENU  */}
+      <div
+        className="relative flex flex-col justify-center items-center md:hidden p-2 space-y-1 rounded border border-gray-700 border-opacity-0 hover:border-opacity-100 duration-200 rotate-360"
+        onClick={handleBurgerMenu}
+      >
+        <span
+          className={`${
+            isOpen ? "-rotate-45 translate-y-2 bg-gray-800" : "bg-gray-400"
+          } w-7 h-1 rounded duration-300`}
+        ></span>
+        <span
+          className={`${
+            isOpen ? "opacity-0" : "bg-gray-400"
+          } w-7 h-1 rounded duration-300`}
+        ></span>
+        <span
+          className={`${
+            isOpen ? "rotate-45 -translate-y-2 bg-gray-800" : "bg-gray-400"
+          } w-7 h-1 rounded duration-300`}
+        ></span>
+      </div>
+      {/* MOBILE NAVBAR  */}
+      <div
+        className={`${
+          isOpen ? "right-0" : "-right-full hidden"
+        } absolute flex flex-col md:hidden pt-10 text-center top-20 w-full h-screen text-2xl bg-gray-100 duration-500`}
+        onClick={handleBurgerMenu}
+      >
+        <Link
+          href="/transactions"
+          className="text-gray-800 font-semibold w-1/2 mx-auto p-2 mb-5 border-b-2 border-gray-700 border-opacity-0 hover:border-opacity-100 duration-200"
+        >
+          Transactions
+        </Link>
+        <Link
+          href="/accountInfo"
+          className="text-gray-800 font-semibold w-1/2 mx-auto p-2 mb-7 border-b-2 border-gray-700 border-opacity-0 hover:border-opacity-100 duration-200"
+        >
+          Account settings
+        </Link>
+        <div
+          onClick={logoutHandler}
+          className="flex justify-center items-center w-1/2 mx-auto cursor-pointer text-gray-800 font-semibold ease-linear transition-all duration-200 p-2 border-2 rounded-lg mt-5 border-gray-700"
+        >
+          <MdLogout className="hidden md:block" />
+          <p>Logout </p>
+        </div>
       </div>
     </div>
   );
--- components/transactions/components/RecentTransactions.jsx
+++ components/transactions/components/RecentTransactions.jsx
@@ -3,7 +3,7 @@ import TransactionCard from "./TransactionCard";
 
 const RecentTransactions = ({ transactions }) => {
   return (
-    <div className="w-full lg:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-y-scroll">
+    <div className="w-full lg:col-span-2 lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-y-scroll">
       <h1 className="text-black font-bold lg:text-2xl md:text-xl text-lg">
         Recent Transactions
       </h1>
