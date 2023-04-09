2caa46abaef375af4c0c256becc42be74501896c
ConstantinHuidu <constantin.huidu@gmail.com>
2023-04-09 14:44:19 +0300

    new login screen

--- components/Login.jsx
+++ components/Login.jsx
@@ -6,6 +6,7 @@ import LoadingSpinner from "./generic/LoadingSpinner";
 import { validateEmail } from "@/helpers/auth";
 import { CustomInput, DefaultButton } from "./generic/GenericComponents";
 import Toaster from "./generic/Toaster";
+import Image from "next/image";
 
 const defaultErrorState = {
   error: false,
@@ -72,44 +73,54 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-gray-100">
-      <form
-        onSubmit={submitFormHandler}
-        noValidate
-        className="flex flex-col justify-center items-center max-w-4xl m-auto align-middle h-[650px] w-10/12"
-      >
-        <CustomInput
-          labelFor="email"
-          inputType="email"
-          labelName="E-mail"
-          onHandleChange={handleEmailChange}
+    <div className="flex justify-around items-center w-[95%] mx-auto">
+      <div className="hidden lg:block relative w-1/2 h-[80vh]">
+        <Image
+          alt="login"
+          src="https://images.unsplash.com/photo-1579621970343-21c491b3f363?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bW9uZXl8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          fill
+          className="object-cover rounded-3xl p-5"
         />
-        <CustomInput
-          labelFor="password"
-          inputType="password"
-          labelName="Password"
-          onHandleChange={handlePasswordChange}
-        />
-        <Link
-          href="/signup"
-          className="text-sm underline text-blue-600 hover:text-blue-800 visited:text-purple-600 my-5"
+      </div>
+      <div className="w-[90%] my-5 mx-auto lg:w-1/2 lg:my-0 border border-gray-300 rounded-lg h-[75vh] ">
+        <form
+          onSubmit={submitFormHandler}
+          noValidate
+          className="flex flex-col justify-center items-center max-w-4xl m-auto align-middle h-[650px] w-10/12"
         >
-          Don't have an account? Create one
-        </Link>
-        {!isLoading && <DefaultButton buttonText="Log in" />}
-        {isLoading && (
-          <DefaultButton buttonText="Loading..." isDisabled={true}>
-            <LoadingSpinner />
-          </DefaultButton>
+          <CustomInput
+            labelFor="email"
+            inputType="email"
+            labelName="E-mail"
+            onHandleChange={handleEmailChange}
+          />
+          <CustomInput
+            labelFor="password"
+            inputType="password"
+            labelName="Password"
+            onHandleChange={handlePasswordChange}
+          />
+          <Link
+            href="/signup"
+            className="text-sm underline text-blue-600 hover:text-blue-800 visited:text-purple-600 my-5"
+          >
+            Don't have an account? Create one
+          </Link>
+          {!isLoading && <DefaultButton buttonText="Log in" />}
+          {isLoading && (
+            <DefaultButton buttonText="Loading..." isDisabled={true}>
+              <LoadingSpinner />
+            </DefaultButton>
+          )}
+        </form>
+        {showToaster && (
+          <Toaster
+            title={taskStatus.statusMessage}
+            status={taskStatus.error ? "❌" : "✔"}
+            color={taskStatus.error ? "red" : "green"}
+          />
         )}
-      </form>
-      {showToaster && (
-        <Toaster
-          title={taskStatus.statusMessage}
-          status={taskStatus.error ? "❌" : "✔"}
-          color={taskStatus.error ? "red" : "green"}
-        />
-      )}
+      </div>
     </div>
   );
 };
