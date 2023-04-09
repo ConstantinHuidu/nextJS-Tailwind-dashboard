295a491116d2f69edb5465bed8fc48b03c827411
ConstantinHuidu <constantin.huidu@gmail.com>
2023-04-09 18:40:12 +0300

    home page draft v1

---
+++ components/homePage/HomePage.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import React from "react";
+import homePage from "../../assets/images/homePage.png";
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col space-y-10 md:space-y-0 md:space-x-5 md:flex-row justify-start items-start w-[90%] h-[1020px] md:h-[80vh] my-5 bg-white mx-auto">
+      <div className="flex flex-col md:flex-row justify-between items-center max-w-7xl mx-auto w-full h-[550px] p-5">
+        <div className="hidden relative md:block w-full md:w-2/3 h-full">
+          <Image
+            alt="home"
+            src={homePage}
+            fill
+            className="object-cover rounded-3xl"
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center md:items-start w-full md:w-1/3 h-full space-y-7 md:pl-10">
+          <h1 className="text-5xl text-gray-800 font-bold text-center">
+            More than just an expense tracker
+          </h1>
+          <p className="text-gray-400 text-md font-semibold text-center md:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            consequatur consectetur esse et non rem veniam aspernatur atque,
+            quam a blanditiis nostrum illum error dolore quis? Ut corrupti
+            commodi ex.
+          </p>
+          <Link
+            href="/transactions"
+            className="bg-cyan-700 py-3 px-10 rounded-full text-white text-xl"
+          >
+            Get started
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
-import TopCards from "@/components/TopCards";
-import BarChart from "@/components/BarChart";
-import RecentOrders from "@/components/RecentOrders";
+
 import { getSession } from "next-auth/react";
+import HomePage from "@/components/homePage/HomePage";
 
 export default function Home() {
   return (
@@ -13,13 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-100 min-h-screen">
-        <TopCards />
-        <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
-          <BarChart />
-          <RecentOrders />
-        </div>
-      </main>
+      <HomePage />
     </>
   );
 }
@@ -28,14 +21,14 @@ export default function Home() {
 export async function getServerSideProps(context) {
   const session = await getSession({ req: context.req });
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: "/login",
+  //       permanent: false,
+  //     },
+  //   };
+  // }
 
   return {
     props: { session },
